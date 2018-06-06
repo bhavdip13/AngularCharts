5e3a4eafb87d7d685596d7ab606e9f1056dcd3a9
talaviya bhavdip <talaviya.bhavdip@outlook.com>
2018-06-06 18:35:51 +0530

    Add project files.

---
+++ AngularCharts/ClientApp/app/components/charts/line-chart/line-chart.component.ts
@@ -0,0 +1,100 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+	selector: 'app-line-chart',
+	templateUrl: './line-chart.component.html',
+	styleUrls: ['./line-chart.component.css']
+})
+
+
+export class LineChartComponent {
+	public weatherForecast: WeatherForecast;
+	public chartType: string = 'line';
+
+	public chartLegend: boolean = true;
+
+
+	public chartOptions: any = {
+		responsive: true,
+		legend: {
+			position: 'bottom'
+		}
+	};
+
+	constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+		http.get(baseUrl + 'api/LineChart/GetWeatherForecast').subscribe(result => {
+			this.weatherForecast = result.json() as WeatherForecast;
+		}, error => console.error(error));
+	}
+	public chartColors: Array<any> = [
+		{
+
+			//backgroundColor: 'rgba(255, 0, 0, 0.2)',
+			borderColor: 'rgba(255,153,51,1)',
+			pointBackgroundColor: 'rgba(0,128,0,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+			pointRadius: 8,
+			fill: false,
+		},
+		{
+
+			//backgroundColor: 'rgba(0, 0, 255, 0.4)',
+			borderColor: 'rgba(77,83,96,1)',
+			pointBackgroundColor: 'rgba(77,83,96,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(77,83,96,1)',
+			lineTension: 0,
+			pointRadius: 10,
+			pointStyle: 'rectRounded',
+			fill: false,
+		},
+		{
+			//backgroundColor: 'green',
+			borderColor: 'rgba(0,128,0,1)',
+			pointBackgroundColor: 'rgba(148,159,177,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+			pointStyle: 'rect',
+			pointRadius: 6,
+			fill: false,
+
+		},
+
+		{
+
+			//backgroundColor: 'rgba(255, 255, 0, 0.2)',
+			borderColor: 'rgba(0,0,255,1)',
+			pointBackgroundColor: 'rgba(0,0,255,1)',
+			pointBorderColor: '#fff',
+			pointHoverBackgroundColor: '#fff',
+			pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+			pointStyle: 'triangle',
+			pointRadius: 10,
+			fill: false,
+		}
+	];
+
+	public showLineChart() {
+		this.chartType = 'line';
+	}
+
+	public showBarChart() {
+		this.chartType = 'bar';
+	}
+}
+
+interface Weather {
+	data: Array<number>;
+	label: string;
+}
+
+interface WeatherForecast {
+	weatherList: Weather[];
+
+}
+
---
+++ AngularCharts/Controllers/BarChartController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Enum;
+
+namespace AngularCharts.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/BarChart")]
+    public class BarChartController : Controller
+    {
+
+        //[HttpGet("[action]")]
+        //public Weather GetBarChartData()
+        //{
+
+        //    var dict = new Dictionary<string, int[]>();
+
+        //    dict.Add("New Delhi",new int[] { 6, 19, 6, 21, 7, 21, 8 });
+        //    dict.Add("New York", new int[] { -8, -6, -1, 2, -7, 6, 4 });
+
+        //    var tempratutes = new List<CityWeather>();
+        //    tempratutes.Add(new CityWeather { Temprature = new int[] { 6, 19, 6, 21, 7, 21, 8 }, CityName = "New Delhi" });
+        //    tempratutes.Add(new CityWeather { Temprature = new int[] { -8, -6, -1, 2, -7, 6, 4 }, CityName = "New York" });
+
+        //    List<string> days = new List<string>();
+
+        //    foreach (DayOfWeek day in GetValues(typeof(DayOfWeek)))
+        //    {
+        //        if (day == DayOfWeek.Wednesday)
+        //        {
+        //            days.Add(day + " (Lowest)");
+        //            break;
+        //        }
+        //        days.AddRange(new string[] { day + " (Lowest)", day + " (Highest)" });
+        //    }
+
+        //    return new Weather { CitiesWeather = citiesWeather, Days = days.ToArray() };
+
+        //}
+
+
+        [HttpGet("[action]")]
+        public Weather GetChartData()
+        {
+
+            var citiesWeather = new List<CityWeather>();
+            citiesWeather.Add(new CityWeather { CityName = "New Delhi", Temprature = new int[] { 6, 19, 6, 21, 7, 21, 8 } });
+            citiesWeather.Add(new CityWeather { CityName = "New York", Temprature = new int[] { -8, -6, -1, 2, -7, 6, 4 } });
+
+            List<string> days = new List<string>();
+
+            foreach (DayOfWeek day in GetValues(typeof(DayOfWeek)))
+            {
+                if (day == DayOfWeek.Wednesday)
+                {
+                    days.Add($"{day} (Lowest)");
+                    break;
+                }
+                days.AddRange(new string[] { $"{day} (Lowest)", $"{day} (Highest)" });
+            }
+
+            return new Weather { CitiesWeather = citiesWeather, Days = days.ToArray() };
+
+        }
+    }
+
+
+    public class CityWeather
+    {
+        public int[] Temprature { get; set; }
+        public string CityName { get; set; }
+    }
+
+    public class Weather
+    {
+        public List<CityWeather> CitiesWeather { get; set; }
+        public string[] Days { get; set; }
+    }
+
+
+    public class WeatherForecast
+    {
+        public string DateFormatted { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+
+        public int TemperatureF
+        {
+            get
+            {
+                return 32 + (int)(TemperatureC / 0.5556);
+            }
+        }
+    }
+}
